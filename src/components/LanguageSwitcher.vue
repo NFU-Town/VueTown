60f3187851f9fbedad4b5cf19d92cb2f6a62581e
WuDreamer <162408159+WuDreamer@users.noreply.github.com>
2024-09-12 11:27:54 +0800

    language

---
+++ src/components/LanguageSwitcher.vue
@@ -0,0 +1,38 @@
+<template>
+  <select
+    @change="changeLanguage"
+    v-model="currentLocale"
+    class="language-selector"
+  >
+    <option value="zh">中文</option>
+    <option value="en">English</option>
+    <option value="ja">日本語</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "LanguageSwitcher",
+  data() {
+    return {
+      currentLocale: this.$i18n.locale,
+    };
+  },
+  methods: {
+    changeLanguage(event) {
+      this.$i18n.locale = event.target.value;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.language-selector {
+  position: relative;
+  top: 50px;
+  margin-left: 2px;
+  border-radius: 4px; /* 边框圆角 */
+  padding: 3px 1px; /* 内边距 */
+  cursor: pointer; /* 鼠标指针样式 */
+}
+</style>
